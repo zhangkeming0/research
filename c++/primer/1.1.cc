f6b493a26426d4ab1b62b5c34144d54e413677af
zhangkeming0 <zhangkeming0@163.com>
2024-06-28 00:05:55 +0800

    feat: really need practice c++

--- c++/primer/1.1.cc
+++ c++/primer/1.1.cc
@@ -1,13 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int v1=0, v2=0;
-    cin >> v1 >> v2;
-    cout << "the sum of v1" << v1
-         << " and v2" << v2
-         << " is " << v1 + v2
-         << endl;
-    return 0;
+/**
+ * @brief 主函数
+ *
+ * 程序的入口函数。
+ *
+ * @return 返回值
+ */
+int main(int argc, char *argv[]) {
+  // 定义变量v1和v2并初始化为0
+  int v1 = 0, v2 = 0;
+  // 从标准输入读取v1和v2的值
+  cin >> v1 >> v2;
+  // 输出v1和v2的和
+  cout << "the sum of v1" << v1 << " and v2" << v2 << " is " << v1 + v2 << endl;
+  // 返回0表示程序正常结束
+  return 0;
 }
